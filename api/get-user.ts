48998ec26e4988cc0dd278bbe763226c369b7173
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-10-31 16:21:08 +0700

    Fix

--- api/get-user.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "../src/utils/supabaseClient";
-
-export default async function getUser(req, res) {
-  const user = await supabase.auth.user();
-  console.log('user', user)
-  return res.status(200).json({ user });
-}
